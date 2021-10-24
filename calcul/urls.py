af9a330c8c484d0ec5da7d42b5b16746a3127334
chikhaoui <chikhaoui.faycel@gmail.com>
2021-10-24 23:55:51 +0200

    logo added

--- calcul/urls.py
+++ calcul/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from django.urls.conf import include
 
 urlpatterns = [
+    path('', include('calculator.urls')),
     path('admin/', admin.site.urls),
-    path('calcul/', include('calculator.urls')),
 ]
